4b9c9ea978a69d6381d22ec7b6e26ccc82e39619
akankshap18 <118830103+akankshap18@users.noreply.github.com>
2023-10-25 19:23:55 +0530

    Update sorting.js

--- JavaScript/sorting.js
+++ JavaScript/sorting.js
@@ -1,1 +1,20 @@
+function sortDescending(arr) {
+    arr.sort(function(a, b) {
+        return b - a; 
+    });
+    return arr;
+}
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter elements of the array : ', (input) => {
+  const elements = input.split(' ').map(Number);
+  const sortedArray = sortDescending(elements);
+  console.log('Sorted Array in Descending Order:', sortedArray);
+  rl.close();
+});
