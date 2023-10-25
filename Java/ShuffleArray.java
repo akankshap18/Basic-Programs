f5adfbf15ae8d4d058bf7389c84360ddd6c4afc4
akankshap18 <118830103+akankshap18@users.noreply.github.com>
2023-10-25 19:21:08 +0530

    Update ShuffleArray.java

--- Java/ShuffleArray.java
+++ Java/ShuffleArray.java
@@ -1,1 +1,25 @@
+import java.util.Random;
 
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+
+        System.out.println("Shuffled Array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void shuffleArray(int[] array) {
+        Random random = new Random();
+
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+}
